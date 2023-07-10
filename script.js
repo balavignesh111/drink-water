a244ca994db6bcb702713f479cca509179e046cd
balavignesh111 <balavignesh1018@gmail.com>
2023-07-10 20:38:26 +0530

    js part completed

--- script.js
+++ script.js
@@ -1,14 +1,37 @@
 const smallGlass = document.querySelectorAll('.smallGlass');
+const waterBottleElement = document.querySelector('.remaining-water-ltr');
+const smallWaterBottleElement = document.querySelector('.water-filling');
 
-console.log(smallGlass); 
+// console.log(smallGlass); 
+// console.log(smallWaterBottleElement)
+// gv
+let height = 0;
 
+const updateHeight = function(){
+  smallWaterBottleElement.style.height = `${height}rem`;
+  waterBottleElement.style.height = `${(20 - height)}rem`;
+}
 const addWater = function(smallGlass){
-  smallGlass.classList.add('waterFilled');
+  if(!(smallGlass.classList.contains('waterFilled'))){
+    smallGlass.classList.add('waterFilled');
+    height += 2.5;
+    if(height === 20){
+      waterBottleElement.style.display= 'none';
+    }
+  }
+  updateHeight();
   smallGlass.addEventListener('click',()=>{removeWater(smallGlass)});
 }
 
 const removeWater = function(smallGlass){
-  smallGlass.classList.remove('waterFilled');
+  if(smallGlass.classList.contains('waterFilled')){
+    smallGlass.classList.remove('waterFilled');
+    height -= 2.5;
+    if(height < 20){
+      waterBottleElement.style.display= 'flex';
+    }
+  }
+  updateHeight();
   smallGlass.addEventListener('click',()=>{addWater(smallGlass)});
 }
 
